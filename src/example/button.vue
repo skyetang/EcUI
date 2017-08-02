db5b7ee6004c6b539f72aeff4e66caae0cd1b2fb
SkyeTang <dengjc@ecworking.com>
2017-08-02 15:35:03 +0800

    edit css

--- src/example/button.vue
+++ src/example/button.vue
@@ -2,8 +2,8 @@
   <div>
     <ec-button>默认</ec-button>
     <ec-button><i class="icon-password icon-margin-right"></i>详情</ec-button>
-    <ec-button type="plain-danger">删除</ec-button>
-    <ec-button type="plain-default">关闭</ec-button>
+    <ec-button >关闭</ec-button>
+    <ec-button type="danger">删除</ec-button>
     <ec-button :disabled="true">失效</ec-button>
   </div>
 </template>

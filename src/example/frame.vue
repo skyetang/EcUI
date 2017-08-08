773f29b4deca5a2e91306c97a86c8bd69356d3a2
SkyeTang <dengjc@ecworking.com>
2017-08-08 12:15:17 +0800

    add class

--- src/example/frame.vue
+++ src/example/frame.vue
@@ -7,20 +7,21 @@
     <ec-container>
       <ec-container-item width="400px">
         <template slot="head">
-          <ec-text @click="addNew"><i class="icon-hp_list__add icon-left" ></i>新增</ec-text>
+          <ec-text><i class="icon-hp_list__add icon-left" ></i>新增</ec-text>
         </template>
-
+        <ec-menu>
+          <ec-menu-item index="1">
+            <div class="flex-space"><span>菜单1</span><span>12</span></div>
+          </ec-menu-item>
+          <ec-menu-item index="2">
+            <div class="flex-space"><span>菜单2</span><span>13</span></div>
+          </ec-menu-item>
+        </ec-menu>
       </ec-container-item>
       <ec-container-item>
         <template slot="head">
-          <ec-text @click="addNew"><i class="icon-hp_list__add icon-left" ></i>新增</ec-text>
+          <ec-text><i class="icon-hp_list__add icon-left" ></i>新增</ec-text>
         </template>
-        <ec-menu-item index="1">
-          菜单1
-        </ec-menu-item index="2">
-        <ec-menu-item>
-          菜单2
-        </ec-menu-item>
         <template slot="footer">
         <ec-pagination :total-page="10"></ec-pagination>
       </template>

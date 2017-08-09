2906b1c12763eb0c5a42c36425cf8aa95ceadcb3
SkyeTang <dengjc@ecworking.com>
2017-08-09 19:28:46 +0800

    add animation.

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="info-page">
-    <div class="page-wrapper">
-      <div class="page-header">
-        <div class="header-item"><i class="gray icon-return" @click="close"></i></div>
-        <div class="header-item">
-          <slot name="title">
-            {{title}}
+  <transition name="page-fade">
+    <div class="info-page">
+      <div class="page-wrapper">
+        <div class="page-header">
+          <div class="header-item"><i class="gray icon-return" @click="close"></i></div>
+          <div class="header-item">
+            <slot name="title">
+              {{title}}
+          </slot>
+          </div>
+          <div class="header-item"><i class="gray icon-innerpage__close" @click="close"></i></div>
+        </div>
+        <div class="page-body">
+          <div class="page-content">
+            <slot></slot>
+          </div>
+        </div>
+        <div class="page-footer">
+          <slot name="footer">
+            <ec-button>取消</ec-button>
+            <ec-button>保存</ec-button>
           </slot>
         </div>
-        <div class="header-item"><i class="gray icon-innerpage__close" @click="close"></i></div>
-      </div>
-      <div class="page-body">
-        <div class="page-content">
-          <slot></slot>
-        </div>
-      </div>
-      <div class="page-footer">
-        <slot name="footer">
-          <ec-button>取消</ec-button>
-          <ec-button>保存</ec-button>
-        </slot>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
